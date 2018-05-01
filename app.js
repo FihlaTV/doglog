c71c3b0df1742ac3d5313b987b38ed76c03ceee6
Katy <ihatetoast@gmail.com>
2018-05-01 12:37:34 -0500

    traded yargs for inquirer. add dog, get pack done

--- app.js
+++ app.js
@@ -1,30 +1,115 @@
 const fs = require('fs');
-const yargs = require('yargs');
-// const argv = yargs
-//   .command('list', 'List all commands')
-//   .command('add', 'Add a note', { titleOptions, bodyOptions })
-//   .command('read', 'Read a note', { titleOptions })
-//   .command('remove', 'Remove a note', { titleOptions })
-//   .help().argv;
-
-const argv = yargs.argv;
+const inquirer = require('inquirer');
+const colors = require('colors/safe');
 //own files
-const dogs = require('./doglogic');
-console.log('Welcome to DogLog.');
-console.log('add (+args): add a dog to the pack');
-console.log('pack: get all dogs (a pack) from DogLog');
-console.log('fetch + name: get a dog from the pack');
-console.log('down: remove a dog from the pack');
+const commands = require('./doglogic');
 
-if (command === 'add') {
-  console.log('Dog added to DogLog');
-} else if (command === 'pack') {
-  console.log('Show all of dog pack');
-} else if (command === 'fetch') {
-  console.log('Single dog retrieved from pack');
-} else if (command === 'down') {
-  console.log('Dog removed');
-} else {
-  console.log('Err: command was not recognised.');
-  console.log('Did you mean add, pack, fetch, or down');
-}
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'commands',
+      message: 'What do you want to do? \n',
+      choices: [
+        'Add a hound',
+        'Remove a hound',
+        'Fetch a hound',
+        'See the pack'
+      ]
+    }
+  ])
+  .then(function(answer) {
+    var command = answer.commands;
+    switch (command) {
+      case 'See the pack':
+        const allHounds = commands.getPack();
+        console.log('Calling the entire pack:');
+        console.log(`Printing ${allHounds.length} hounds:`);
+        allHounds.forEach(hound => commands.printInfo(hound));
+        break;
+      case 'Remove a hound':
+        inquirer
+          .prompt([
+            {
+              name: 'hound',
+              message: 'Which hound do you wish to remove from the pack? \n'
+            }
+          ])
+          .then(function(answer) {
+            var name = answer.hound;
+            commands.deleteHound(name);
+          });
+        break;
+      case 'Fetch a hound':
+        inquirer
+          .prompt([
+            {
+              name: 'hound',
+              message: 'Which hound do you wish fetch? \n'
+            }
+          ])
+          .then(function(answer) {
+            var name = answer.hound;
+            commands.getHound(name);
+          });
+        break;
+      case 'Add a hound':
+        inquirer
+          .prompt([
+            {
+              name: 'hound',
+              message: 'What is the name of the hound? \n'
+            },
+            {
+              type: 'list',
+              name: 'color',
+              message: `What color type is the ${colors.rainbow('hound')}? \n`,
+              choices: [
+                'Brindle',
+                'Black Brindle',
+                'Black and White',
+                'Blue',
+                'Blue Brindle',
+                'Light Brindle',
+                'Dark Brindle',
+                'Fawn Brindle',
+                'Fawn',
+                'Dark Red',
+                'Black',
+                'Light Red Fawn',
+                'White and Brindle',
+                'Red Fawn',
+                'Red and White',
+                'White and Brindle Tick',
+                'Red Brindle',
+                'White and Black',
+                'Greyhound X'
+              ]
+            },
+            {
+              name: 'sex',
+              type: 'list',
+              message: 'Bitch or dog? \n',
+              choices: ['bitch', 'dog']
+            },
+            {
+              name: 'description',
+              message:
+                "Write a brief description of the hound's personality. \n"
+            }
+          ])
+          .then(function(answer) {
+            var name = answer.hound;
+            var color = answer.color;
+            var sex = answer.sex;
+            var description = answer.description;
+            commands.addHound(name, color, sex, description);
+            console.log('Hound added: ');
+            commands.printInfo(hound);
+          });
+        break;
+      default:
+        console.clear();
+        console.log(`Sorry, I do not recognise ${command}.`);
+    }
+  });
--- doglogic.js
+++ doglogic.js
@@ -1,1 +1,68 @@
 const fs = require('fs');
+
+var getLogEntries = () => {
+  try {
+    //save as ..
+    var logString = fs.readFileSync('doglog-datalog.json');
+    return JSON.parse(logString);
+  } catch (err) {
+    return [];
+  }
+};
+
+var saveLogEntries = entries => {
+  fs.writeFileSync('doglog-datalog.json', JSON.stringify(entries));
+};
+//get all of the entries:
+const getPack = () => {
+  return getLogEntries();
+};
+//delete a hound
+const deleteHound = name => {
+  var pack = getLogEntries();
+  var stayHounds = pack.filter(hound => pack.name !== name);
+  saveLogEntries(stayHounds);
+  return pack.length !== stayHounds.length;
+};
+//get one hound:
+const getHound = name => {
+  var pack = getLogEntries();
+  var targetHound = pack.filter(hound => pack.name === name);
+  return targetHound[0];
+};
+
+//add a hound to the pack:
+const addHound = (name, color, sex, description) => {
+  var pack = getLogEntries();
+  var hound = {
+    name,
+    color,
+    sex,
+    description
+  };
+  var doopHound = pack.filter(hound => hound.name === name);
+
+  if (doopHound.length === 0) {
+    pack.push(hound);
+    saveLogEntries(pack);
+    return hound;
+  }
+};
+const printInfo = hound => {
+  if (!hound) {
+    console.log('Hound not found.');
+  } else {
+    console.log('~~~~~~~~~~');
+    console.log(`Name: ${hound.name}`);
+    console.log(`body: ${hound.color}`);
+    console.log(`Name: ${hound.sex}`);
+    console.log(`body: ${hound.description}`);
+  }
+};
+module.exports = {
+  addHound,
+  getPack,
+  getHound,
+  deleteHound,
+  printInfo
+};
