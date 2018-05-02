55b49586add1f89175b01cb851b2de757e780bb9
Katy <ihatetoast@gmail.com>
2018-05-01 20:46:04 -0500

    added demo gif. fixed table issues. DONE

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 
 Dog Log is a simple node journaling app to keep track of incoming foster dogs because the turnover from foster to adopted out can make it hard to remember them.
 
-Using just the command line, I can enter a dog's name, color, sex, and basic personality description.
+Using just the command line, I can enter a dog's name, color, sex, and basic personality description. I can also delete a dog from the list and retrieve all or one.
 
-## Toolbox:
+## Demo:
+
+![DogLog Demo](doglog.gif)
+
+### Toolbox:
 
 * NodeJS
 * Inquirer
--- doglogic.js
+++ doglogic.js
@@ -15,38 +15,40 @@ var getLogEntries = () => {
 var saveLogEntries = entries => {
   fs.writeFileSync('doglog-datalog.json', JSON.stringify(entries));
 };
-const table = new Table({
-  chars: {
-    top: '═',
-    'top-mid': '╤',
-    'top-left': '╔',
-    'top-right': '╗',
-    bottom: '═',
-    'bottom-mid': '╧',
-    'bottom-left': '╚',
-    'bottom-right': '╝',
-    left: '║',
-    'left-mid': '╟',
-    mid: '─',
-    'mid-mid': '┼',
-    right: '║',
-    'right-mid': '╢',
-    middle: '│'
-  },
-  style: { 'padding-left': 0, 'padding-right': 0 },
-  colWidths: [5, 90]
-});
+
 //show as initial caps:
 String.prototype.initCap = function() {
   const replacement = this.charAt(0).toUpperCase();
   return replacement + this.substr(0 + replacement.length);
 };
+
 const printInfo = hound => {
   if (!hound) {
     console.log('Hound not found.');
   } else {
+    const table = new Table({
+      chars: {
+        top: '═',
+        'top-mid': '╤',
+        'top-left': '╔',
+        'top-right': '╗',
+        bottom: '═',
+        'bottom-mid': '╧',
+        'bottom-left': '╚',
+        'bottom-right': '╝',
+        left: '║',
+        'left-mid': '╟',
+        mid: '─',
+        'mid-mid': '┼',
+        right: '║',
+        'right-mid': '╢',
+        middle: '│'
+      },
+      style: { 'padding-left': 0, 'padding-right': 0 },
+      colWidths: [5, 90]
+    });
     table.push(
-      { Name: colors.rainbow(hound.name.initCap()) },
+      { Name: colors.magenta(hound.name.initCap()) },
       { Color: hound.color },
       { Sex: hound.sex },
       { Desc: hound.description }
@@ -72,7 +74,6 @@ const deleteHound = name => {
 const getHound = name => {
   var pack = getLogEntries();
   var targetHound = pack.filter(hound => hound.name === name);
-  console.log(targetHound);
   return targetHound[0];
 };
 
